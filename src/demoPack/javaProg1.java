51fe2f3251a74a1a924dfaefb6919ede23ba19e2
Alka Singh <alka.k.singh@apisero.com>
2022-07-26 15:58:01 +0530

    First Commit

---
+++ src/demoPack/javaProg1.java
@@ -0,0 +1,13 @@
+package demoPack;
+
+public class javaProg1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		System.out.println("This is the simple java program");
+		// TODO Auto-generated method stub
+
+	}
+
+}

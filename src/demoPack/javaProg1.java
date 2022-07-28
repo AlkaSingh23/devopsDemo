f0e6825f25f5d65e6c32c7125243ef130bfa4693
Alka Singh <alka.k.singh@apisero.com>
2022-07-28 20:18:20 +0530

    Commit by 8

--- src/demoPack/javaProg1.java
+++ src/demoPack/javaProg1.java
@@ -6,8 +6,23 @@ public class javaProg1 {
 	{
 		System.out.println("Hello World");
 		System.out.println("This is the simple java program");
+		
+		int r,sum=0,temp;    
+		  int n=454;//It is the number variable to be checked for palindrome  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+		
 		// TODO Auto-generated method stub
 
 	}
 
-}
